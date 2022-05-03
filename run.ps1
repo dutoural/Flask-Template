7e694e4026187db9ecf24c3da7c8917c7658a5e7
Alexandre DUTOUR <63452813+dutoural@users.noreply.github.com>
2022-05-03 11:50:45 +0900

    Add auto-run

--- run.ps1
+++ run.ps1
@@ -1,3 +1,4 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 .venv\Scripts\activate
 set FLASK_APP=app.py
+python run.py
--- setup.ps1
+++ setup.ps1
@@ -2,3 +2,5 @@ python -m venv .venv
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 .venv\Scripts\activate
 pip install -r app/config/requirements.txt
+set FLASK_APP=app.py
+python run.py
